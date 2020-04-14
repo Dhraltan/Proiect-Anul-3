ecf0879acd58a744d7ee59e9c74a0634b36f0a4e
Dhraltan <gcatalins98@gmail.com>
2020-04-14 15:21:44 +0300

    Implemented design for the login section

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express'); //We bring in express
 const router = express.Router(); //We create router in order to use the express router
 
-router.get('/login',(req,res) => res.send('Login')); //Handels the login page, has an arrow function that sends Login as a response 
+router.get('/login',(req,res) => res.render('login')); //Handels the login page, has an arrow function that renders the login view
 
-router.get('/register',(req,res) => res.send('Register')); //Handels the register page, has an arrow function that sends Register as a response 
+router.get('/register',(req,res) => res.render('register')); //Handels the register page, has an arrow function that renders the register view
 
 
 module.exports = router; //Exports the path as a module
