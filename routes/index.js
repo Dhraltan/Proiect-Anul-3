e33f6979de0cafe3b1794c531ba5ae9234f5df01
Dhraltan <gcatalins98@gmail.com>
2020-04-14 13:34:49 +0300

    Refacerea proiectului in Angular

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express'); //We bring in express
-const router = express.Router(); //We create router in order to use the express router
-
-router.get('/',(req,res) => res.render('welcome')); //Handels the home page, has an arrow function that renders the welcome view as a response 
-
-module.exports = router; //Exports the path as a module
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); //We bring in express
-const router = express.Router(); //We create router in order to use the express router
-
-router.get('/login',(req,res) => res.send('Login')); //Handels the login page, has an arrow function that sends Login as a response 
-
-router.get('/register',(req,res) => res.send('Register')); //Handels the register page, has an arrow function that sends Register as a response 
-
-
-module.exports = router; //Exports the path as a module
